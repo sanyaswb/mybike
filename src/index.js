84c8280ec0dbdf3c7fb324734a3b3eead8278579
Oleksandr Merkulov <odin@terradeus.org>
2025-09-20 19:12:31 +0300

    add animations for images to details and events

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector(".menu");
 const menuOpen = document.querySelector(".icon--menu");
 const menuClose = document.querySelector(".icon--close");
+const sectionsDetailImages = document.querySelectorAll(".detail__images");
+const detailImage = document.querySelectorAll(".detail__image");
 
 menuOpen.addEventListener("click", () => {
   menu.classList.add("menu--open");
@@ -8,4 +10,21 @@ menuOpen.addEventListener("click", () => {
 
 menuClose.addEventListener("click", () => {
   menu.classList.remove("menu--open");
+});
+
+sectionsDetailImages.forEach((section) => {
+  const links = section.querySelectorAll(".detail__link");
+
+  links.forEach((link) => {
+    link.addEventListener("mouseenter", () => {
+      if (link.classList.contains("detail__link--wide")) return;
+      links.forEach((l) => l.classList.toggle("detail__link--wide"));
+    });
+  });
+});
+
+detailImage.forEach((image) => {
+  image.addEventListener("click", () => {
+    image.classList.toggle("detail__image--scale");
+  });
 });
